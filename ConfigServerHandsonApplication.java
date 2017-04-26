8f4d86ab6f34daacc5caa9f9aa8df6c4dc0c0029
Takayuki Kaburagi <tkaburagi@pivotal.io>
2017-04-26 13:33:01 +0900

    Update ConfigServerHandsonApplication.java

--- ConfigServerHandsonApplication.java
+++ ConfigServerHandsonApplication.java
@@ -1,3 +1,13 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 @RestController
 public class ConfigServerHandsonApplication {	
